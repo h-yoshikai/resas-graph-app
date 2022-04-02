ca46b934f33d118789a0d05ea2bf8483611d4a5a
h-yoshikai <exbass11@gmail.com>
2022-04-02 22:43:40 +0900

    feat: 配列データをもとにチェックボックスを表示

--- src/components/SelectPrefectureArea/SelectPrefectureArea.jsx
+++ src/components/SelectPrefectureArea/SelectPrefectureArea.jsx
@@ -1,8 +1,42 @@
 export const SelectPrefectureArea = () => {
   return (
     <div>
-      <input type="checkbox" name="prefecture" id="prefCode1" />
-      <label htmlFor="prefCode1">北海道</label>
+      {PREFECTURE_LIST.result.map((pref) => (
+        <div key={pref.prefCode}>
+          <input
+            type="checkbox"
+            name="prefecture"
+            id={`prefCode${pref.prefCode}`}
+          />
+          <label htmlFor={`prefCode${pref.prefCode}`}>{pref.prefName}</label>
+        </div>
+      ))}
     </div>
   );
+};
+
+const PREFECTURE_LIST = {
+  message: null,
+  result: [
+    {
+      prefCode: 1,
+      prefName: "北海道",
+    },
+    {
+      prefCode: 2,
+      prefName: "青森県",
+    },
+    {
+      prefCode: 3,
+      prefName: "岩手県",
+    },
+    {
+      prefCode: 4,
+      prefName: "宮城県",
+    },
+    {
+      prefCode: 5,
+      prefName: "秋田県",
+    },
+  ],
 };
