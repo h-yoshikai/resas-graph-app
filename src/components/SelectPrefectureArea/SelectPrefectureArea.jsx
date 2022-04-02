5a9d637c6fbde2ccb5e5074f364508e0cff1a9aa
h-yoshikai <exbass11@gmail.com>
2022-04-02 18:24:45 +0900

    feat: 都道府県一覧コンポーネントの雛型の作成

---
+++ src/components/SelectPrefectureArea/SelectPrefectureArea.jsx
@@ -0,0 +1,8 @@
+export const SelectPrefectureArea = () => {
+  return (
+    <div>
+      <input type="checkbox" name="prefecture" id="prefCode1" />
+      <label htmlFor="prefCode1">北海道</label>
+    </div>
+  );
+};
