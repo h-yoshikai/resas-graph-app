2bf5fc66c112400876a05fa77c515d6f998f1428
h-yoshikai <exbass11@gmail.com>
2022-04-02 22:53:32 +0900

    refactor: 都道府県一覧をpropsのデータをもとに表示できるように変更

--- src/components/SelectPrefectureArea/SelectPrefectureArea.jsx
+++ src/components/SelectPrefectureArea/SelectPrefectureArea.jsx
@@ -1,9 +1,9 @@
 import PrefectureCheckBox from "@/src/components/PrefectureCheckBox";
 
-export const SelectPrefectureArea = () => {
+export const SelectPrefectureArea = ({ prefectureList }) => {
   return (
     <div>
-      {PREFECTURE_LIST.result.map((pref) => (
+      {prefectureList.map((pref) => (
         <PrefectureCheckBox
           key={pref.prefCode}
           prefCode={pref.prefCode}
@@ -12,30 +12,4 @@ export const SelectPrefectureArea = () => {
       ))}
     </div>
   );
-};
-
-const PREFECTURE_LIST = {
-  message: null,
-  result: [
-    {
-      prefCode: 1,
-      prefName: "北海道",
-    },
-    {
-      prefCode: 2,
-      prefName: "青森県",
-    },
-    {
-      prefCode: 3,
-      prefName: "岩手県",
-    },
-    {
-      prefCode: 4,
-      prefName: "宮城県",
-    },
-    {
-      prefCode: 5,
-      prefName: "秋田県",
-    },
-  ],
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,33 @@ import SelectPrefectureArea from "@/src/components/SelectPrefectureArea";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <SelectPrefectureArea />
+      <SelectPrefectureArea prefectureList={PREFECTURE_LIST.result} />
     </div>
   );
 }
+
+const PREFECTURE_LIST = {
+  message: null,
+  result: [
+    {
+      prefCode: 1,
+      prefName: "北海道",
+    },
+    {
+      prefCode: 2,
+      prefName: "青森県",
+    },
+    {
+      prefCode: 3,
+      prefName: "岩手県",
+    },
+    {
+      prefCode: 4,
+      prefName: "宮城県",
+    },
+    {
+      prefCode: 5,
+      prefName: "秋田県",
+    },
+  ],
+};
