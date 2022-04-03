c35a98e2564ceeb50e579edc8e10d2077ae40fe3
h-yoshikai <exbass11@gmail.com>
2022-04-03 22:06:17 +0900

    style: チェックボックス部分をカード化

--- src/components/PrefectureCheckBox/PrefectureCheckBox.jsx
+++ src/components/PrefectureCheckBox/PrefectureCheckBox.jsx
@@ -1,13 +1,17 @@
+import classes from "@/src/components/PrefectureCheckBox/PrefectureCheckBox.module.css";
+
 export const PrefectureCheckBox = ({ prefCode, prefName, onChange }) => {
   return (
-    <div>
+    <div className={classes.checkbox}>
       <input
         type="checkbox"
         name="prefecture"
         id={`prefCode${prefCode}`}
         onChange={(e) => onChange(prefCode, prefName, e.target.checked)}
       />
-      <label htmlFor={`prefCode${prefCode}`}>{prefName}</label>
+      <label htmlFor={`prefCode${prefCode}`}>
+        {prefName.length === 3 ? prefName + "　" : prefName}
+      </label>
     </div>
   );
 };
